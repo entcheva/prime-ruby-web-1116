c178b42504903c06b2accefaa1d3e0f410701417
Christina Entcheva <christina.entcheva@gmail.com>
2016-11-19 06:05:35 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+def prime?(int)
+  counter = 2
+  prime = true
+
+  if int < 2
+    return false
+  end
+
+  while counter < int
+
+    if int % counter == 0
+      return false
+    end
+    counter += 1
+  end
+
+  return prime
+
+end
